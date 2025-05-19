91a86363e8e847ed86ae0740dbfc9e3c95768a6e
Suheldevs <mohdsuhel.dev@gmail.com>
2025-05-19 18:46:09 +0530

    home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import HomeCardSlide from '../components/Home/homeCardSlide'
+
 import CategoryProductGrid from '../components/Home/CategoryProductGrid'
 import SmallBannerSlider from '../components/Home/SmallBannerSlider'
+import HomeCardSlide from '../components/Home/HomeCardSlider'
 
 export default function Home() {
   return (
